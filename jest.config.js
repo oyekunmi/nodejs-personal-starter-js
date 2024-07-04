b4607551e55ff46d5af9a4eb8ce4fe2db613cece
Oyesola Ogundele <oyekunmi@gmail.com>
2024-07-04 15:28:33 +0400

    feat(test): automatic coverage reporting on tests

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,8 @@ const config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["src/**"],
+  // collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
